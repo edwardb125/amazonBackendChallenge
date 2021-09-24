baa85501f9a1d2220207ca62a4334305edded5b6
erfan hemati <erfanhematig@gmail.com>
2021-09-24 21:27:56 +0330

    some test commented

--- controllers/setDevice_test.go
+++ controllers/setDevice_test.go
@@ -1,72 +1,71 @@
 package controllers
-//
-//import (
-//	//"./createDynamoDB"
-//	"amazonBackendChallenge/models"
-//	"bytes"
-//	"encoding/json"
-//	"github.com/go-playground/assert/v2"
-//	"github.com/gorilla/mux"
-//	"net/http"
-//	"net/http/httptest"
-//	"os"
-//	"testing"
-//)
-//
-//func TestGetDeviceController(t *testing.T) {
-//	_ = os.Unsetenv("AWS_REGION")
-//	_ = os.Unsetenv("TABLE_NAME")
-//	input := models.Device{
-//		Id:          "ididid",
-//		DeviceModel: "test",
-//		Name:        "test",
-//		Note:        "test",
-//		Serial:      "test",
-//	}
-//	tests := []struct {
-//		name   string
-//		input  models.Device
-//		status int
-//		output interface{}
-//	}{
-//		{name: "server error", input: input, status: 500, output: Error{
-//			Message: "server error",
-//		}},
-//		{name: "input is invalid", input: models.Device{
-//			Id: "1",
-//		}, status: 400, output: Error{
-//			Message: "invalid device info",
-//		}},
-//		{name: "well done", input: input, status: 201, output: input},
-//	}
-//
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			if test.name == "ok" {
-//				_ = os.Setenv("AWS_REGION", "us-west-2")
-//				_ = os.Setenv("TABLE_NAME", "Devices")
-//			}
-//			router := mux.NewRouter()
-//			router.HandleFunc("/devices", SetDevice).Methods("POST")
-//
-//			marshal, _ := json.Marshal(test.input)
-//			req, _ := http.NewRequest(http.MethodPost, "/devices", bytes.NewBuffer(marshal))
-//
-//			res := httptest.NewRecorder()
-//			router.ServeHTTP(res, req)
-//			assert.Equal(t, test.status, res.Code)
-//
-//			if res.Code == 201 {
-//				var device models.Device
-//				_ = json.Unmarshal(res.Body.Bytes(), &device)
-//				assert.Equal(t, test.output.(models.Device), device)
-//			} else {
-//				var err Error
-//				_ = json.Unmarshal(res.Body.Bytes(), &err)
-//				assert.Equal(t, test.output.(Error), err)
-//			}
-//		})
-//	}
-//	DeleteDeviceId(t, input.Id)
-//
-//}
+
+import (
+	"amazonBackendChallenge/models"
+	"bytes"
+	"encoding/json"
+	"github.com/go-playground/assert/v2"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestGetDeviceController(t *testing.T) {
+	_ = os.Unsetenv("AWS_REGION")
+	_ = os.Unsetenv("TABLE_NAME")
+	input := models.Device{
+		Id:          "ididid",
+		DeviceModel: "test",
+		Name:        "test",
+		Note:        "test",
+		Serial:      "test",
+	}
+	tests := []struct {
+		name   string
+		input  models.Device
+		status int
+		output interface{}
+	}{
+		{name: "server error", input: input, status: 500, output: Error{
+			Message: "server error",
+		}},
+		{name: "input is invalid", input: models.Device{
+			Id: "1",
+		}, status: 400, output: Error{
+			Message: "invalid device info",
+		}},
+		{name: "well done", input: input, status: 201, output: input},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.name == "ok" {
+				_ = os.Setenv("AWS_REGION", "us-west-2")
+				_ = os.Setenv("TABLE_NAME", "Devices")
+			}
+			router := mux.NewRouter()
+			router.HandleFunc("/devices", SetDevice).Methods("POST")
+
+			marshal, _ := json.Marshal(test.input)
+			req, _ := http.NewRequest(http.MethodPost, "/devices", bytes.NewBuffer(marshal))
+
+			res := httptest.NewRecorder()
+			router.ServeHTTP(res, req)
+			assert.Equal(t, test.status, res.Code)
+
+			if res.Code == 201 {
+				var device models.Device
+				_ = json.Unmarshal(res.Body.Bytes(), &device)
+				assert.Equal(t, test.output.(models.Device), device)
+			} else {
+				var err Error
+				_ = json.Unmarshal(res.Body.Bytes(), &err)
+				assert.Equal(t, test.output.(Error), err)
+			}
+		})
+	}
+	DeleteDeviceId(t, input.Id)
+
+}
--- service/getDevice_test.go
+++ service/getDevice_test.go
@@ -1,45 +1,45 @@
 package service
-//
-//import (
-//	"amazonBackendChallenge/mocks"
-//	"amazonBackendChallenge/models"
-//	"errors"
-//	"github.com/aws/aws-sdk-go/aws"
-//	"github.com/aws/aws-sdk-go/service/dynamodb"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/stretchr/testify/mock"
-//	"testing"
-//)
-//func TestGetDevice(t *testing.T) {
-//	item:=map[string]*dynamodb.AttributeValue{"id":&dynamodb.AttributeValue{S: aws.String("1")}}
-//	tests := []struct {
-//		name           string
-//		item           map[string]*dynamodb.AttributeValue
-//		getItemError   error
-//		errorExpected  error
-//		outputExpected models.Device
-//	}{
-//		{name: "device not found",errorExpected: errors.New("device not found")},
-//		{name: "get some errore in getItem",getItemError: errors.New("err"),errorExpected: errors.New("server error")},
-//		{name: "well done",item: item,outputExpected: models.Device{Id: "1"}},
-//	}
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			mockDB := new(mocks.DeviceDynamoDB)
-//			mockDB.On("GetItem", mock.Anything).Return(&dynamodb.GetItemOutput{
-//				Item: test.item,
-//			}, test.getItemError)
-//			service := &GetCore{
-//				Db: mockDB,
-//			}
-//
-//			output, err := service.GetDevice("")
-//			if err == nil {
-//				assert.Nil(t, test.errorExpected)
-//			} else {
-//				assert.Error(t, test.errorExpected, err.Error())
-//			}
-//			assert.Equal(t, test.outputExpected, output)
-//		})
-//	}
-//}
+
+import (
+	"amazonBackendChallenge/mocks"
+	"amazonBackendChallenge/models"
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+func TestGetDevice(t *testing.T) {
+	item:=map[string]*dynamodb.AttributeValue{"id":&dynamodb.AttributeValue{S: aws.String("1")}}
+	tests := []struct {
+		name           string
+		item           map[string]*dynamodb.AttributeValue
+		getItemError   error
+		errorExpected  error
+		outputExpected models.Device
+	}{
+		{name: "device not found",errorExpected: errors.New("device not found")},
+		{name: "get some errore in getItem",getItemError: errors.New("err"),errorExpected: errors.New("server error")},
+		{name: "well done",item: item,outputExpected: models.Device{Id: "1"}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockDB := new(mocks.DeviceDynamoDB)
+			mockDB.On("GetItem", mock.Anything).Return(&dynamodb.GetItemOutput{
+				Item: test.item,
+			}, test.getItemError)
+			service := &GetCore{
+				Db: mockDB,
+			}
+
+			output, err := service.GetDevice("")
+			if err == nil {
+				assert.Nil(t, test.errorExpected)
+			} else {
+				assert.Error(t, test.errorExpected, err.Error())
+			}
+			assert.Equal(t, test.outputExpected, output)
+		})
+	}
+}

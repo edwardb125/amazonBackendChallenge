5c2300f8bfc83f33e627e9d7b75722936d66d7e1
erfan hemati <erfanhematig@gmail.com>
2021-09-26 14:03:33 +0330

    some comment added

--- service/createDevice_test.go
+++ service/createDevice_test.go
@@ -17,7 +17,7 @@ func TestCreateService(t *testing.T) {
 		putItemError  error
 		errorExpected error
 	}{
-		// test 2 position
+		// 2 test for done and server error
 		{name: "well done"},
 		{name:"service error occurred",putItemError: errors.New("error"),errorExpected: errors.New("server error")},
 	}
